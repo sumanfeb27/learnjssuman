02d769040a54362c10d8c2a1dc98cadca3f66743
learnjssuman <sumanfeb27@gmail.com>
2025-04-29 23:20:56 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,75 @@
+sample=10
+console.log(sample)
+console.log(typeof(sample))
+
+sample=true
+console.log(sample)
+console.log(typeof(sample))
+
+sample="Raju"
+console.log(sample)
+console.log(typeof(sample))
+
+st1="raju"
+console.log(typeof(st1))
+
+st2='suman'
+console.log(typeof(st2))
+
+st3=`ram`
+console.log(typeof(st3))
+
+//primitiv data types 
+
+num=10.5
+console.log(typeof(num))//number
+
+x=true
+y=false
+
+console.log(typeof(x))//Boolean
+
+var dep
+console.log(typeof(dep))
+
+var a = undefined
+console.log(typeof(a))//undefined 
+
+b=null
+console.log(typeof(b))//object
+
+
+//non primitive data types 
+
+arr["admin","admin123"]
+
+console.log(arr)
+
+arr1[1,3,4,6,8,9]
+
+console.log(arr1)
+
+creds={
+    usrename:"admin",
+    password:"admin123"
+    
+}
+
+console.log(creds.usrename)
+
+car={
+    name:"Honda",
+    color:"blue",
+    year:"2025"
+
+}
+
+console.log(car.name)
+
+
+
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,53 @@
+var x=10
+var y=20
+
+z=x+y
+
+console.log(z)
+
+//var name="raju" global scope
+
+//const name="raju" blocked scope
+
+//let name="raju " blocked scope 
+
+var greet="Hi"
+
+if(5>3){
+
+    console.log(greet);
+
+}
+// with var keyword reassigne the value is possible 
+var stuname="suman"
+
+stuname="Jay"
+
+console.log(stuname)
+
+// with let keyword reassigne the value is possible 
+
+if (true){
+    let trainer="raju"
+    trainer="murali"
+    console.log(trainer)
+}
+
+// // with const keyword reassigne the value is not possible 
+// if (true){
+//     const trainer="raju"
+//     trainer="murali"
+//     console.log(trainer)
+// }
+
+// var is available from the inistall Version
+//let and const from es6 Version
+//re decleration same varible is  possible with var
+var ordernm='990099'
+var ordernm='999999'
+console.log(ordernm)
+//re decleration same varible is not possible with let and const 
+// let ordernm='22220'
+// let ordernm='33333'
+// console.log(ordernm)
+
