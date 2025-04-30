153c73c7f060c33fd5089a00a005abe0b6767b78
learnjssuman <sumanfeb27@gmail.com>
2025-04-30 15:25:28 +0530

    operaters

---
+++ operaters.js
@@ -0,0 +1,69 @@
+a=10
+b=20
+c=a+b
+console.log(c)
+console.log(20-10)
+console.log(8/8)
+console.log(10%3)
+x=20
+x++ //incerment operater 
+console.log(x)
+
+y=30
+y-- //decrment operater 
+console.log(y)
+
+//compersion operaters == only compare values not the data types 
+
+console.log(2==2)
+
+console.log(5==3)
+
+//=== compare values and data types as well 
+
+console.log(4==4)//true 
+console.log("5"===5)//fale
+
+//<> 
+
+console.log(4>4)//false
+console.log(4>=4)//true
+console.log(4!=4)
+console.log(2==="2")&&(4<3)
+console.log(2==="2")||(4<3)
+
+//assignment operaters 
+
+d=20
+d+=10 //d=d+10
+
+console.log(d)
+
+//condentional operator or ternary operater
+
+e="ram"
+f="sri"
+g= 4=="4"? e:f
+console.log(g)
+
+workers=4>3 ? 2:1
+console.log(workers)
+
+price1=20
+price2=30
+total=50
+console.log(price1+price2==total)
+
+//coersion 
+
+h=6+3//9 value adding 
+
+str="suman" +"V" //sumanv concatanation 
+
+sum=6+"4"
+console.log(sum)
+
+value="suman" + 5
+console.log(value)
+
+console.log(10+20+30+"45"+10+20)
